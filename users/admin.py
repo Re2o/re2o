d0e76bdf4b41625e5bb0c744f172437c78875364
Gabriel Detraz <detraz@crans.org>
2016-07-01 01:03:28 +0200

    Corrige des bugs , change le type de etat à integer, permet la creation d'un user de manière complète

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, School
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name','surname','pseudo','email', 'school', 'state')
+
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(School, SchoolAdmin)
