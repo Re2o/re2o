539f252a22d5844b4a16f9e6e456d7b8ef3c4844
lhark <lhark@arachne>
2016-07-02 00:38:12 +0200

    Add minimal logs app and root index page

--- re2o/urls.py
+++ re2o/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from logs.views import index
+
 urlpatterns = [
+    url(r'^$', index),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^users/', include('users.urls')),
     url(r'^search/', include('search.urls')),
