b721a482505aac22972bb404c02681572d98538b
lhark <lhark@arachne>
2016-07-02 00:38:12 +0200

    Add minimal logs app and root index page

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^new_user/$', views.new_user, name='new_user'),
-    url(r'^edit_info/(?P<userid>[0-9]+)$', views.edit_info, name='edit_info'),
-    url(r'^state/(?P<userid>[0-9]+)$', views.state, name='state'),
-    url(r'^password/(?P<userid>[0-9]+)$', views.password, name='password'),
+    url(r'^new_user/$', views.new_user, name='users-new-user'),
+    url(r'^edit_info/(?P<userid>[0-9]+)$', views.edit_info, name='users-edit-info'),
+    url(r'^state/(?P<userid>[0-9]+)$', views.state, name='users-state'),
+    url(r'^password/(?P<userid>[0-9]+)$', views.password, name='users-password'),
+    url(r'^$', views.index, name='users-index'),
 ]
 
 
