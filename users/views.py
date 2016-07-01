998289ce5ae94ed858e068e855c0a41f0321ad66
Gabriel Detraz <detraz@crans.org>
2016-07-01 22:47:08 +0200

    Ajoute un menu pour changer l'etat et le mot de passe

--- users/views.py
+++ users/views.py
@@ -1,13 +1,14 @@
 # App de gestion des users pour re2o
 # Goulven Kermarec, Gabriel Détraz
 # Gplv2
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core.context_processors import csrf
 from django.template import Context, RequestContext, loader
+from django.contrib import messages
 
 from users.models import User, UserForm, InfoForm, PasswordForm, StateForm
-
+from users.forms  import PassForm
 
 def form(ctx, template, request):
     c = ctx
@@ -28,8 +29,33 @@ def edit_info(request, userid):
     try:
         user = User.objects.get(pk=userid)
     except User.DoesNotExist:
-        user = None
+        messages.error(request, u"Utilisateur inexistant" )
+        return redirect("/users/")
     user = InfoForm(request.POST or None, instance=user)
     if user.is_valid():
         user.save()
     return form({'userform': user}, 'users/user.html', request)
+
+def state(request, userid):
+    try:
+        user = User.objects.get(pk=userid)
+    except User.DoesNotExist:
+        messages.error(request, u"Utilisateur inexistant" )
+        return redirect("/users/")
+    user = StateForm(request.POST or None, instance=user)
+    if user.is_valid():
+        user.save()
+    return form({'userform': user}, 'users/user.html', request)
+
+def password(request, userid):
+    try:
+        user = User.objects.get(pk=userid)
+    except User.DoesNotExist:
+        messages.error(request, u"Utilisateur inexistant" )
+        return redirect("/users/")
+    user_form = PassForm(request.POST or None)
+    if user_form.is_valid():
+        user.pwd_ssha = user_form.cleaned_data['passwd']
+        user.pwd_ntlm = user_form.cleaned_data['passwd'] 
+        user.save()
+    return form({'userform': user_form}, 'users/user.html', request)
