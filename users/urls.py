f22c098cccb61e6b8e5179704ae513c52809d141
lhark <lhark@arachne>
2016-07-02 02:07:50 +0200

    Namespacing now handled by root urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^new_user/$', views.new_user, name='users-new-user'),
-    url(r'^edit_info/(?P<userid>[0-9]+)$', views.edit_info, name='users-edit-info'),
-    url(r'^state/(?P<userid>[0-9]+)$', views.state, name='users-state'),
-    url(r'^password/(?P<userid>[0-9]+)$', views.password, name='users-password'),
-    url(r'^$', views.index, name='users-index'),
+    url(r'^new_user/$', views.new_user, name='new-user'),
+    url(r'^edit_info/(?P<userid>[0-9]+)$', views.edit_info, name='edit-info'),
+    url(r'^state/(?P<userid>[0-9]+)$', views.state, name='state'),
+    url(r'^password/(?P<userid>[0-9]+)$', views.password, name='password'),
+    url(r'^add_right/$', views.add_right, name='add-right'),
+    url(r'^$', views.index, name='index'),
 ]
 
 
