6159f7b20804e5a09dd049b40fae0f3320e08daa
chirac <chirac@arachne>
2016-07-02 18:30:59 +0200

    Ajoute une table cotisation spécifique

--- cotisations/admin.py
+++ cotisations/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-from .models import Facture, Article, Banque, Paiement
+from .models import Facture, Article, Banque, Paiement, Cotisation
 
 class FactureAdmin(admin.ModelAdmin):
     list_display = ('user','paiement','name', 'number', 'date')
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('name','prix')
+    list_display = ('name','prix','cotisation')
 
 class BanqueAdmin(admin.ModelAdmin):
     list_display = ('name',)
@@ -14,7 +14,14 @@ class BanqueAdmin(admin.ModelAdmin):
 class PaiementAdmin(admin.ModelAdmin):
     list_display = ('moyen',)
 
+class PaiementAdmin(admin.ModelAdmin):
+    list_display = ('moyen',)
+
+class CotisationAdmin(admin.ModelAdmin):
+    list_display = ('facture','date_start','date_end')
+
 admin.site.register(Facture, FactureAdmin)
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Banque, BanqueAdmin)
 admin.site.register(Paiement, PaiementAdmin)
+admin.site.register(Cotisation, CotisationAdmin)
