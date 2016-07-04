50945f0dfa555df0cc8f130bff9a0730a94f289a
Dalahro <Dalahro@arachne>
2016-07-04 23:56:51 +0200

    Ajout de la recherche avancée

--- search/views.py
+++ search/views.py
@@ -7,10 +7,10 @@ from django.core.context_processors import csrf
 from django.template import Context, RequestContext, loader
 
 from django.db.models import Q
-from users.models import User, Ban
+from users.models import User, Ban, Whitelist
 from machines.models import Machine, Interface
 from cotisations.models import Facture
-from search.models import SearchForm
+from search.models import SearchForm, SearchFormPlus
 from users.views import has_access
 
 def form(ctx, template, request):
@@ -18,25 +18,72 @@ def form(ctx, template, request):
     c.update(csrf(request))
     return render_to_response(template, c, context_instance=RequestContext(request))
 
+def search_result(search, type):
+    date_deb = None
+    date_fin = None 
+    states=[]
+    co=[]
+    aff=[0,1,2,3,4]
+    if(type):
+        aff = search.cleaned_data['affichage']
+        co = search.cleaned_data['connexion']
+        states = search.cleaned_data['filtre']
+        date_deb = search.cleaned_data['date_deb']
+        date_fin = search.cleaned_data['date_fin']
+    date_query = Q()
+    if aff==[]:
+        aff = [0,1,2,3,4]
+    if date_deb != None:
+        date_query = date_query & Q(date__gte=date_deb)
+    if date_fin != None:
+        date_query = date_query & Q(date__lte=date_fin)
+    search = search.cleaned_data['search_field']
+    query = Q() 
+    for s in states:
+        query = query | Q(state = s)
+    
+    users = None
+    machines = None
+    factures = None
+    bans = None
+    whitelists = None
+    connexion = []
+
+    for i in aff:
+        if i == '0':
+            users = User.objects.filter((Q(pseudo__icontains = search) | Q(name__icontains = search) | Q(surname__icontains = search)) & query)
+            connexion = []
+            for user in users:
+                access=has_access(user)
+                if(len(co)==0 or (len(co)==1 and bool(co[0])==access) or (len(co)==2 and (bool(co[0])==access or bool(co[1])==access))):
+                    connexion.append([user, access])
+        query = Q(user__pseudo__icontains = search) | Q(user__name__icontains = search) | Q(user__surname__icontains = search)
+        if i == '1':
+            machines = Interface.objects.filter(machine=Machine.objects.filter(query)) | Interface.objects.filter(Q(dns__icontains = search))
+        if i == '2':   
+            factures = Facture.objects.filter(query & date_query)
+        if i == '3':    
+            bans = Ban.objects.filter(query)
+        if i == '4':    
+            whitelists = Whitelist.objects.filter(query)
+    return {'users_list': connexion, 'machine_list' : machines, 'facture_list' : factures, 'ban_list' : bans, 'white_list': whitelists}
+
 def search(request):
     if request.method == 'POST':
         search = SearchForm(request.POST or None)
         if search.is_valid():
-            states = search.cleaned_data['filtre']
-            search = search.cleaned_data['search_field']
-            query = Q() 
-            for s in states:
-                query = query | Q(state = s)
-            users = User.objects.filter((Q(pseudo__icontains = search) | Q(name__icontains = search) | Q(surname__icontains = search)) & query)
-            connexion = []
-            for user in users:
-                connexion.append([user, has_access(user)])
-            query = Q(user__pseudo__icontains = search) | Q(user__name__icontains = search) | Q(user__surname__icontains = search)
-            machines = Interface.objects.filter(machine=Machine.objects.filter(query)) | Interface.objects.filter(Q(dns__icontains = search))
-            factures = Facture.objects.filter(query)
-            bans = Ban.objects.filter(query)
-            return form({'users_list': connexion, 'machine_list' : machines, 'facture_list' : factures, 'ban_list' : bans}, 'search/index.html',request)
+            return form(search_result(search, False), 'search/index.html',request)
         return form({'searchform' : search}, 'search/search.html', request)
     else:
         search = SearchForm(request.POST or None) 
+        return form({'searchform': search}, 'search/search.html',request)
+
+def searchp(request):
+    if request.method == 'POST':
+        search = SearchFormPlus(request.POST or None)
+        if search.is_valid():
+            return form(search_result(search, True), 'search/index.html',request)
+        return form({'searchform' : search}, 'search/search.html', request)
+    else:
+        search = SearchFormPlus(request.POST or None) 
         return form({'searchform': search}, 'search/search.html',request)
