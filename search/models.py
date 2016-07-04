17719418f2451c0f5683d84f8bd7bd1648ecf7b2
Dalahro <Dalahro@arachne>
2016-07-04 23:56:51 +0200

    Ajout de la recherche avancée

--- search/models.py
+++ search/models.py
@@ -12,8 +12,27 @@ CHOICES = (
     ('2', 'Archivés'),
 )
 
+CHOICES2 = (
+    (1, 'Active'),
+    ("", 'Désactivée'),
+)
+
+CHOICES3 = (
+    ('0', 'Utilisateurs'),
+    ('1', 'Machines'),
+    ('2', 'Factures'),
+    ('3', 'Bannissements'),
+    ('4', 'Accès à titre gracieux'),
+)
+ 
+
 class SearchForm(Form):
+    search_field = forms.CharField(label = 'Search', max_length = 100)
+
+class SearchFormPlus(Form):
     search_field = forms.CharField(label = 'Search', max_length = 100, required=False)
-    filtre = forms.MultipleChoiceField(label="Filtre utilisateur", required=False, widget =forms.CheckboxSelectMultiple,choices=CHOICES)
+    filtre = forms.MultipleChoiceField(label="Filtre utilisateurs", required=False, widget =forms.CheckboxSelectMultiple,choices=CHOICES)
+    connexion = forms.MultipleChoiceField(label="Filtre connexion", required=False, widget =forms.CheckboxSelectMultiple,choices=CHOICES2)
+    affichage = forms.MultipleChoiceField(label="Filtre affichage", required=False, widget =forms.CheckboxSelectMultiple,choices=CHOICES3)
     date_deb = forms.DateField(required=False, label="Date de début", help_text='DD/MM/YYYY', input_formats=['%d/%m/%Y'])
     date_fin = forms.DateField(required=False, help_text='DD/MM/YYYY', input_formats=['%d/%m/%Y'], label="Date de fin")
