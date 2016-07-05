ffb22e96786a088c4286395f02eab052cc926388
Gabriel Detraz <detraz@crans.org>
2016-07-05 16:30:46 +0200

    Passe les dates dans le bon fuseau horraire, en type fr

--- re2o/settings.py
+++ re2o/settings.py
@@ -75,9 +75,9 @@ WSGI_APPLICATION = 're2o.wsgi.application'
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
