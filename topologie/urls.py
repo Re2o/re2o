09d5e1697518a9351af493264b009e2b61308c0c
Dalahro <Dalahro@arachne>
2016-07-06 23:29:31 +0200

    Topologie: ajout et modif

--- topologie/urls.py
+++ topologie/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^new_switch/$', views.new_switch, name='new-switch'),
     url(r'^switch/(?P<switch_id>[0-9]+)$', views.index_port, name='index-port'),
     url(r'^edit_port/(?P<port_id>[0-9]+)$', views.edit_port, name='edit-port'),
+    url(r'^new_port/(?P<switch_id>[0-9]+)$', views.new_port, name='new-port'),
+    url(r'^edit_switch/(?P<switch_id>[0-9]+)$', views.edit_switch, name='edit-switch'),
 ]
 
