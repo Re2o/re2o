6d1b41ff3107e42a0ef1c9fd0f40bf1fb362fd60
chirac <chirac@arachne>
2016-07-06 20:57:31 +0200

    Vues pour gérer la liste des articles

--- cotisations/urls.py
+++ cotisations/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^new_facture/(?P<userid>[0-9]+)$', views.new_facture, name='new-facture'),
     url(r'^edit_facture/(?P<factureid>[0-9]+)$', views.edit_facture, name='edit-facture'),
+    url(r'^add_article/$', views.add_article, name='add-article'),
+    url(r'^del_article/$', views.del_article, name='del-article'),
     url(r'^$', views.index, name='index'),
 ]
 
