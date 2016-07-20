13d4e2e79504d5763ab5e04ec734ed4ca981b9e0
chirac <chirac@arachne>
2016-07-20 12:18:37 +0200

    Ménage, mise en var du nom re2o

--- re2o/settings_local.example.py
+++ re2o/settings_local.example.py
@@ -19,6 +19,8 @@ DATABASES = {
 
 # Association information
 
+SITE_NAME = "Re2o.rez"
+
 LOGO_PATH = "static_files/logo.png"
 ASSO_NAME = "Asso reseau"
 ASSO_ADDRESS_LINE1 = "2, rue Edouard Belin"
@@ -26,3 +28,15 @@ ASSO_ADDRESS_LINE2 = "57070 Metz"
 ASSO_SIRET = ""
 ASSO_EMAIL = "tresorier@ecole.fr"
 ASSO_PHONE = "01 02 03 04 05"
+
+services_urls = {
+   #Fill IT  : ex :  'gitlab': {'url': 'https://gitlab.rezometz.org', 'logo': 'gitlab.png'},
+    }
+
+# Number of hours a token remains valid after having been created.  Numeric and string
+# versions should have the same meaning.
+REQ_EXPIRE_HRS = 48
+REQ_EXPIRE_STR = '48 heures'
+
+# Email `From` field
+EMAIL_FROM = 'www-data@serveur.net'
