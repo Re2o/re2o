f6a287c4b8033a72dc54bfb73cce24f817aae1ee
Gabriel Detraz <chirac@crans.org>
2016-10-12 12:24:01 +0200

    Corrige l'assignation des UID

---
+++ users/migrations/0039_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0038_user_rezo_rez_uid'),
+        ('users', '0038_auto_20161006_1952'),
+    ]
+
+    operations = [
+    ]
