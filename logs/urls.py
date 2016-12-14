b1a52e619e16707c3269545057bf86032a2ca7ca
Gabriel Detraz <detraz@crans.org>
2016-12-14 03:32:34 +0100

    Depreciation de namespace dans django 1.9

--- logs/urls.py
+++ logs/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'logs'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^revert_action/(?P<revision_id>[0-9]+)$', views.revert_action, name='revert-action'),
