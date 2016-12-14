fc7f1ddb7f64ff85a5995e8457c5b03e4a810b96
root <root@federez-test.crans.org>
2016-12-14 03:34:43 +0100

    Revert "Depreciation de namespace dans django 1.9"
    This reverts commit 2febf5b2467a9734154ee00f572d7799f2c63333.

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'users'
 urlpatterns = [
     url(r'^new_user/$', views.new_user, name='new-user'),
     url(r'^edit_info/(?P<userid>[0-9]+)$', views.edit_info, name='edit-info'),
