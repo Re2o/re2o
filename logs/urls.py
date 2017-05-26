70259cb0e5e95b84f7c301ea02df4c2b26e0cdb9
Gabriel Detraz <detraz@crans.org>
2017-05-26 04:32:45 +0200

    More stats

--- logs/urls.py
+++ logs/urls.py
@@ -28,6 +28,7 @@ app_name = 'logs'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^revert_action/(?P<revision_id>[0-9]+)$', views.revert_action, name='revert-action'),
+    url(r'^stats_general/$', views.stats_general, name='stats-general'),
     url(r'^stats_models/$', views.stats_models, name='stats-models'),
     url(r'^stats_users/$', views.stats_users, name='stats-users'),
     url(r'^stats_actions/$', views.stats_actions, name='stats-actions'),
