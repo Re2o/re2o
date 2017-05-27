5a4203d26e133e2d6d88fd69cdcf05da84690ab5
Gabriel Detraz <detraz@crans.org>
2017-05-27 04:39:53 +0200

    Optimisation sur le menu d'edition d'une machine (accèlère)
    Conflicts:
    	machines/views.py

--- machines/views.py
+++ machines/views.py
@@ -751,7 +751,7 @@ def mac_ip_list(request):
 @login_required
 @permission_required('serveur')
 def alias(request):
-    alias = Domain.objects.filter(interface_parent=None).filter(cname__in=Domain.objects.filter(interface_parent__in=Interface.objects.exclude(ipv4=None)))
+    alias = Domain.objects.filter(interface_parent=None).filter(cname=Domain.objects.filter(interface_parent__in=Interface.objects.exclude(ipv4=None))).select_related('extension')
     seria = DomainSerializer(alias, many=True)
     return JSONResponse(seria.data)
 
