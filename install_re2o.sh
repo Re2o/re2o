58c4fcdcbf7256f83fbcdb40b1a86d82acd7aac3
Gabriel Detraz <detraz@crans.org>
2017-08-31 19:40:24 +0200

    Script d'installation : prise en charge config apache...

--- install_re2o.sh
+++ install_re2o.sh
@@ -1,16 +1,41 @@
+#!/bin/bash
 echo "Installation de Re2o ! 
 Cet utilitaire va procéder à l'installation initiale de re2o. Le serveur présent doit être vierge.
 Preconfiguration..."
-while true; do
-    read -p "Moteur de bdd choisi (mysql ou postgresql)" sql_bdd_type
-    case $sql_bdd_type in
-	    [mysql]* ) break;;
-	    [postgresql]* ) break;;
-    * ) echo "Réponse incorrecte";;
-esac
-done
 
-read -p "Extension locale (ex : example.org)" extension_locale
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get -y install dialog
+
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="Preconfiguration re2o"
+MENU="Choisir une option"
+
+TITLE="Choix du moteur bdd"
+OPTIONS=(1 "mysql"
+         2 "postgresql")
+
+sql_bdd_type=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+
+
+TITLE="Extension locale (ex : example.org)"
+
+extension_locale=$(dialog --title "$TITLE" \
+	--backtitle "$BACKTITLE" \
+        --inputbox "$TITLE" $HEIGHT $WIDTH \
+        2>&1 >/dev/tty)
+clear
+
 IFS='.' read -a extension_locale_array <<< $extension_locale
 
 
@@ -21,21 +46,49 @@ done
 ldap_dn=${ldap_dn::-1}
 echo $ldap_dn
 
-while true; do
-    read -p "Installer la base de donnée sql en local (Y/N)" sql_is_local
-    case $sql_is_local in
-	    [N]* ) echo "Vous devrez fournir un login/mdp/host dans ce cas"; break;;
-	    [Y]* ) break;;
-    * ) echo "Réponse incorrecte (Y/N)";;
-esac
-done
+TITLE="Emplacement de la bdd"
+OPTIONS=(1 "Local"
+         2 "Distant")
 
-read -p "Mot de passe sql " sql_password
-if [ $sql_is_local == "N" ]
+sql_is_local=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+
+TITLE="Mot de passe sql"
+
+sql_password=$(dialog --title "$TITLE" \
+	--backtitle "$BACKTITLE" \
+        --inputbox "$TITLE" $HEIGHT $WIDTH \
+        2>&1 >/dev/tty)
+clear
+
+
+if [ $sql_is_local == 2 ]
 then 
-read -p "Login sql " sql_login
-read -p "Nom bdd sql " sql_name
-read -p "Hote de la base de donnée " sql_host 
+TITLE="Login sql"
+sql_login=$(dialog --title "$TITLE" \
+	--backtitle "$BACKTITLE" \
+        --inputbox "$TITLE" $HEIGHT $WIDTH \
+        2>&1 >/dev/tty)
+clear
+TITLE="Nom de la bdd sql"
+sql_name=$(dialog --title "$TITLE" \
+	--backtitle "$BACKTITLE" \
+        --inputbox "$TITLE" $HEIGHT $WIDTH \
+        2>&1 >/dev/tty)
+clear
+TITLE="Hote de la base de donnée"
+sql_host=$(dialog --title "$TITLE" \
+	--backtitle "$BACKTITLE" \
+        --inputbox "$TITLE" $HEIGHT $WIDTH \
+        2>&1 >/dev/tty)
+clear
 else
 sql_name="re2o"
 sql_login="re2o"
@@ -47,21 +100,41 @@ CREATE USER '$sql_login'@'localhost' IDENTIFIED BY '$sql_password';
 GRANT ALL PRIVILEGES ON $sql_name.* TO '$sql_login'@'localhost';
 FLUSH PRIVILEGES;"
 
-while true; do
-    read -p "Installer la base de donnée ldap en local (Y/N)" ldap_is_local
-    case $ldap_is_local in
-    [N]* ) echo "Vous devrez fournir un login/mdp/host dans ce cas"; break;;
-    [Y]* ) break;;
-    * ) echo "Réponse incorrecte (Y/N)";;
-esac
-done
 
+TITLE="Emplacement du ldap"
+OPTIONS=(1 "Local"
+         2 "Distant")
 
-read -p "Mot de passe ldap " ldap_password
-if [ $ldap_is_local == "N" ]
+ldap_is_local=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+echo "Vous devrez fournir un login/host dans le cas où le ldap est non local"
+
+TITLE="Mot de passe ldap"
+ldap_password=$(dialog --title "$TITLE" \
+	--backtitle "$BACKTITLE" \
+        --inputbox "$TITLE" $HEIGHT $WIDTH \
+        2>&1 >/dev/tty)
+clear
+if [ $ldap_is_local == 2 ]
 then 
-read -p "Cn admin à utiliser " ldap_cn
-read -p "Hote de la base de donnée (adresse ip seulement !)" ldap_host 
+TITLE="Cn ldap admin"
+ldap_cn=$(dialog --title "$TITLE" \
+	--backtitle "$BACKTITLE" \
+        --inputbox "$TITLE" $HEIGHT $WIDTH \
+        2>&1 >/dev/tty)
+clear
+TITLE="Hote ldap"
+ldap_host=$(dialog --title "$TITLE" \
+	--backtitle "$BACKTITLE" \
+        --inputbox "$TITLE" $HEIGHT $WIDTH \
+        2>&1 >/dev/tty)
+clear
 else
 ldap_cn="cn=admin,"
 ldap_cn+=$ldap_dn
@@ -70,17 +143,17 @@ fi
 
 
 echo "Installation des paquets de base"
-export DEBIAN_FRONTEND=noninteractive
 apt-get -y install python3-django python3-dateutil texlive-latex-base texlive-fonts-recommended python3-djangorestframework python3-django-reversion python3-pip libsasl2-dev libldap2-dev libssl-dev
 pip3 install django-bootstrap3
 pip3 install django-ldapdb
 pip3 install django-macaddress
 
-if [ $sql_bdd_type == "mysql" ]
+if [ $sql_bdd_type == 1 ]
 then
-    if [ $sql_is_local == "Y" ]
+    if [ $sql_is_local == 1 ]
     then
     apt-get -y install mysql-server
+    mysql -u root --execute="$sql_command"
     else
     echo "Veuillez saisir la commande suivante sur le serveur sql distant, puis validez"
     echo $sql_command
@@ -95,24 +168,24 @@ then
     fi
     apt-get -y install python3-mysqldb mysql-client
     else
-    if [ $sql_is_local == "Y" ]
+    if [ $sql_is_local == 1 ]
     then
     apt-get -y install postgresql-server
     fi
     apt-get -y install postgresql-client
 fi 
 
-if [ $ldap_is_local == "Y" ]
+if [ $ldap_is_local == 1 ]
 then
 apt-get -y install slapd
 
 echo "Hashage du mot de passe ldap..."
-hashed_ldap_passwd=$(slappasswd -s ldap_password)
+hashed_ldap_passwd=$(slappasswd -s $ldap_password)
 
 echo $hashed_ldap_passwd
 echo "Formatage des fichiers de config ldap"
-sed 's/dc=example,dc=org/'"$ldap_dn"'/g' install_utils/db.ldiff | sed 's/FILL_IT/'"$hashed_ldap_passwd"'/g' > /tmp/db
-sed 's/dc=example,dc=org/'"$ldap_dn"'/g' install_utils/schema.ldiff | sed 's/FILL_IT/'"$hashed_ldap_passwd"'/g' > /tmp/schema
+sed 's|dc=example,dc=org|'"$ldap_dn"'|g' install_utils/db.ldiff | sed 's|FILL_IT|'"$hashed_ldap_passwd"'|g' > /tmp/db
+sed 's|dc=example,dc=org|'"$ldap_dn"'|g' install_utils/schema.ldiff | sed 's|FILL_IT|'"$hashed_ldap_passwd"'|g' > /tmp/schema
 
 echo "Destruction config ldap existante"
 service slapd stop
@@ -135,10 +208,10 @@ fi
 
 echo "Ecriture de settings_local"
 
-django_secret_key=$(python -c 'import random; import string; print "".join([random.SystemRandom().choice(string.digits + string.letters + string.punctuation) for i in range(100)])')
+django_secret_key=$(python -c "import random; print(''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789%=+') for i in range(50)]))")
 
 cp re2o/settings_local.example.py re2o/settings_local.py
-#sed -i 's/SUPER_SECRET_KEY/'"$django_secret_key"'/g' re2o/settings_local.py
+sed -i 's/SUPER_SECRET_KEY/'"$django_secret_key"'/g' re2o/settings_local.py
 sed -i 's/SUPER_SECRET_DB/'"$sql_password"'/g' re2o/settings_local.py
 sed -i 's/db_name_value/'"$sql_name"'/g' re2o/settings_local.py
 sed -i 's/db_user_value/'"$sql_login"'/g' re2o/settings_local.py
@@ -150,5 +223,43 @@ sed -i 's/dc=example,dc=org/'"$ldap_dn"'/g' re2o/settings_local.py
 sed -i 's/example.org/'"$extension_locale"'/g' re2o/settings_local.py
 
 echo "Application des migrations"
-#python3 manage.py migrate
+python3 manage.py migrate
+
+echo "Collecte des statics"
+python3 manage.py collectstatics
+
+BACKTITLE="Fin de l'installation"
+TITLE="Serveur web à utiliser"
+OPTIONS=(1 "apache2"
+         2 "nginx")
+
+web_serveur=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+
+TITLE="Url où servir le serveur web (ex : re2o.example.org)"
+url_server=$(dialog --title "$TITLE" \
+	--backtitle "$BACKTITLE" \
+        --inputbox "$TITLE" $HEIGHT $WIDTH \
+        2>&1 >/dev/tty)
+clear
+
+if [ $web_serveur == 1 ]
+then
+apt-get -y install apache2 libapache2-mod-wsgi-py3
+cp install_utils/apache2/re2o.conf /etc/apache2/sites-available/re2o.conf
+sed -i 's/URL_SERVER/'"$url_server"'/g' /etc/apache2/sites-available/re2o.conf
+current_path=$(pwd)
+sed -i 's|PATH|'"$current_path"'|g' /etc/apache2/sites-available/re2o.conf
+a2ensite re2o
+service apache2 reload
+else
+echo "Nginx non supporté, vous devrez installer manuellement"
+fi
 
