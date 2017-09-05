99c04b9990ab4f8299ffdc943cea181694b59b05
guimoz <rezo@guimoz.fr>
2017-09-05 14:43:40 +0200

    Classement des chambres par orde alphabétique

--- topologie/models.py
+++ topologie/models.py
@@ -135,6 +135,9 @@ class Room(models.Model):
     name = models.CharField(max_length=255, unique=True)
     details = models.CharField(max_length=255, blank=True)
 
+    class Meta:
+        ordering = ['name']
+    
     def __str__(self):
         return str(self.name)
 
