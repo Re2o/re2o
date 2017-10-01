6f0e657ae566132b4cf47027fdfd8d866e949b72
Hugo LEVY-FALK <hugo.levy--falk@supelec.fr>
2017-10-01 16:57:51 +0200

    Enlève les log inutilisables.

--- machines/views.py
+++ machines/views.py
@@ -927,8 +927,6 @@ def edit_portlist(request, pk):
     if port_list.is_valid():
         with transaction.atomic(), reversion.create_revision():
             port_list.save()
-            reversion.set_user(request.user)
-            reversion.set_comment("Champs modifié(s) : %s" % ', '.join(field for field in port_list.changed_data))
         messages.success(request, "Liste de ports modifiée")
         return redirect("/machines/index_portlist/")
     return form({'machineform' : port_list}, 'machines/machine.html', request)
