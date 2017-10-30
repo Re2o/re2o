7c5f81f034c7c1985752f05d8c84dfec475538de
Charlie Jacomme <jacomme@crans.org>
2017-10-30 20:01:16 +0100

    Utilisation de mixins et de middleware pour les révisions

--- re2o/settings.py
+++ re2o/settings.py
@@ -88,6 +88,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'reversion.middleware.RevisionMiddleware',
 )
 
 ROOT_URLCONF = 're2o.urls'
