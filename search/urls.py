9a292d38d866cae3ccf16362cd1400168ef1b7f0
Maël Kervella <kervella.mael@gmail.com>
2017-11-02 19:58:20 +0000

    pep8 et pylint

--- search/urls.py
+++ search/urls.py
@@ -20,6 +20,8 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+"""The urls used by the search app"""
+
 from __future__ import unicode_literals
 
 from django.conf.urls import url
