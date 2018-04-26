b1738f189baacd865a1e0e0bb789cf03d81665b6
Maël Kervella <kervella.mael@gmail.com>
2018-04-26 18:41:13 +0000

    Add pagination to API results

--- api/settings.py
+++ api/settings.py
@@ -34,7 +34,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'api.permissions.DefaultACLPermission',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
 }
 
 # API permission settings
