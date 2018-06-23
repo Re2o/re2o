e9f47677099fa8e7c799e6602453bffb51ccda7d
chirac <chirac@re2o-server.crans.org>
2018-06-23 18:32:43 +0200

    Gestion complete des algo de fingerprintssh

--- machines/models.py
+++ machines/models.py
@@ -236,7 +236,7 @@ class SshFprAlgo(RevMixin, AclMixin, models.Model):
     """Un aglorithme de création de la fingerprint ssh"""
     PRETTY_NAME = "Algo de clef ssh"
 
-    name = models.TextField(max_length=256)
+    name = models.CharField(max_length=256)
 
     class Meta:
         permissions = (
