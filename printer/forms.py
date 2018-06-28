a7d5686f118b1722bc8d125283f61e9d54894e1e
Maxime Bombar <bombar@crans.org>
2018-06-28 20:20:08 +0200

    Printer App a beginning.

---
+++ printer/forms.py
@@ -0,0 +1,37 @@
+# -*- mode: python; coding: utf-8 -*-
+
+"""printer.forms
+Form to add, edit, cancel printer jobs.
+Author : Maxime Bombar <bombar@crans.org>.
+Date : 29/06/2018
+"""
+
+from django import forms
+from django.forms import (
+    Form,
+    ModelForm,
+)
+
+import itertools
+
+from re2o.mixins import FormRevMixin
+
+from .models import (
+    JobWithOptions,
+)
+
+
+class JobForm(FormRevMixin, ModelForm):
+    def __init__(self, *args, **kwargs):
+        prefix = kwargs.pop('prefix', self.Meta.model.__name__)
+        super(TrueJobForm, self).__init__(*args, prefix=prefix, **kwargs)
+
+    class Meta:
+        model = JobWithOptions
+        fields = [
+            'file',
+            'color',
+            'disposition',
+            'count',
+            ]
+                
