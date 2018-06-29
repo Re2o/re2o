3acf992da47b0a4fd9ea4b77a952aeb03f6be0ee
Maxime Bombar <bombar@crans.org>
2018-06-29 05:16:31 +0200

    [printer] Update settings.py (temporary)

--- printer/settings.py
+++ printer/settings.py
@@ -9,5 +9,5 @@ Define variables, to be changed into a configuration table.
 
 
 
-MAX_PRINTFILE_SIZE = 10 * 1024 * 1024 # 25 MB
+MAX_PRINTFILE_SIZE = 25 * 1024 * 1024 # 25 MB
 ALLOWED_TYPES = ['application/pdf']
