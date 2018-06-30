bac18a265de926d338b32b24850893444ae34540
chirac <chirac@raspberrypi>
2018-06-30 15:29:00 +0000

    Ajout et transfert des anciennes données vers le nouveau système de profil de ports

--- topologie/forms.py
+++ topologie/forms.py
@@ -80,8 +80,8 @@ class EditPortForm(FormRevMixin, ModelForm):
     optimiser le temps de chargement avec select_related (vraiment
     lent sans)"""
     class Meta(PortForm.Meta):
-        fields = ['room', 'related', 'machine_interface', 'radius',
-                  'vlan_force', 'details']
+        fields = ['room', 'related', 'machine_interface', 'custom_profil',
+                  'details']
 
     def __init__(self, *args, **kwargs):
         prefix = kwargs.pop('prefix', self.Meta.model.__name__)
@@ -101,8 +101,7 @@ class AddPortForm(FormRevMixin, ModelForm):
             'room',
             'machine_interface',
             'related',
-            'radius',
-            'vlan_force',
+            'custom_profil',
             'details'
         ]
 
