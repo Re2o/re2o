0fe430d72e7a35586a71110fafd1b9882b72502b
Hugo LEVY-FALK <hugo.levy--falk@supelec.fr>
2018-07-02 22:14:51 +0200

    Ajout de comnpay + fix de l'utilisation des paiements personnalisés

--- cotisations/payment_methods/__init__.py
+++ cotisations/payment_methods/__init__.py
@@ -1,25 +1,6 @@
-from django.conf.urls import include, url
-from django.utils.translation import ugettext_lazy as _l
-
-from . import comnpay, cheque
-
-
-urlpatterns = [
-    url(r'^comnpay/', include(comnpay.urls, namespace='comnpay')),
-    url(r'^cheque/', include(cheque.urls, namespace='cheque')),
-]
+from . import comnpay, cheque, urls
 
 PAYMENT_METHODS = [
     comnpay,
     cheque,
 ]
-
-
-def find_payment_method(payment):
-    for method in PAYMENT_METHODS:
-        try:
-            o = method.PaymentMethod.objects.get(payment=payment)
-            return o
-        except method.PaymentMethod.DoesNotExist:
-            pass
-    return None
