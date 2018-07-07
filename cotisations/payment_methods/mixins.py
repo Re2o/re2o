f1661ac2b2af54e8474d5a844a29240669568328
Hugo LEVY-FALK <hugo.levy--falk@supelec.fr>
2018-07-07 20:53:42 +0200

    Suppression d'un champ inutile

--- cotisations/payment_methods/mixins.py
+++ cotisations/payment_methods/mixins.py
@@ -1,15 +1,5 @@
-from django.db import models
-
-from cotisations.models import Paiement
-
-
 class PaymentMethodMixin:
     """The base class for payment models. They should inherit from this."""
-    payment = models.OneToOneField(
-        Paiement,
-        related_name='payment_method',
-        editable=False
-    )
 
     def end_payment(self, invoice, request):
         """Redefine this method in order to get a different ending to the
