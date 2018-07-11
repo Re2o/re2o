c12b3575dd8a6ae18a5672b41c31e46a16d0aee6
Gabriel Detraz <detraz@crans.org>
2018-07-11 03:14:56 +0200

    Bricoles d'affichage

--- preferences/urls.py
+++ preferences/urls.py
@@ -93,13 +93,13 @@ urlpatterns = [
         views.edit_radiuskey,
         name='edit-radiuskey'
         ),
-    url(r'^del_radiuskey/$', views.del_radiuskey, name='del-radiuskey'),
+    url(r'^del_radiuskey/(?P<radiuskeyid>[0-9]+)$', views.del_radiuskey, name='del-radiuskey'),
     url(r'^add_switchmanagementcred/$', views.add_switchmanagementcred, name='add-switchmanagementcred'),
     url(
         r'^edit_switchmanagementcred/(?P<switchmanagementcredid>[0-9]+)$',
         views.edit_switchmanagementcred,
         name='edit-switchmanagementcred'
         ),
-    url(r'^del_switchmanagementcred/$', views.del_switchmanagementcred, name='del-switchmanagementcred'),
+    url(r'^del_switchmanagementcred/(?P<switchmanagementcredid>[0-9]+)$', views.del_switchmanagementcred, name='del-switchmanagementcred'),
     url(r'^$', views.display_options, name='display-options'),
 ]
