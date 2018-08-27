db198c46ad90cef928fe3e828fdfe4f1097a9351
grisel-davy <grisel-davy@crans.org>
2018-08-27 18:57:23 +0200

    Déconfusion entre la personne qui fait l'action et le compte concerné.

--- cotisations/views.py
+++ cotisations/views.py
@@ -827,7 +827,7 @@ def credit_solde(request, user, **_kwargs):
             kwargs={'userid': user.id}
         ))
 
-    refill_form = RechargeForm(request.POST or None, user=user)
+    refill_form = RechargeForm(request.POST or None, user=user, user_source=request.user)
     if refill_form.is_valid():
         price = refill_form.cleaned_data['value']
         invoice = Facture(user=user)
