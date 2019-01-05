20e1ee3bb47dc123cdbd031e0fdf4872364a1a49
Gabriel Detraz <detraz@crans.org>
2019-01-05 16:19:38 +0100

    A granted user can set active at creation

--- users/forms.py
+++ users/forms.py
@@ -387,6 +387,20 @@ class AdherentCreationForm(AdherentForm):
     #gtu_check.label = mark_safe("{} <a href='/media/{}' download='CGU'>{}</a>{}".format(
     #    _("I commit to accept the"), GeneralOption.get_cached_value('GTU'), _("General Terms of Use"), _(".")))
 
+    class Meta:
+        model = Adherent
+        fields = [
+            'name',
+            'surname',
+            'pseudo',
+            'email',
+            'school',
+            'comment',
+            'telephone',
+            'room',
+            'state',
+        ]
+
     def __init__(self, *args, **kwargs):
         super(AdherentCreationForm, self).__init__(*args, **kwargs)
 
