e09aa70a7e32c15143421c84d9a76d352636fc15
Alexandre Iooss <erdnaxe@crans.org>
2019-03-01 11:01:43 +0100

    Clean some templates and respect pagination_number

--- useradmin/admin.py
+++ useradmin/admin.py
@@ -1,4 +1,27 @@
+# -*- mode: python; coding: utf-8 -*-
+# Re2o est un logiciel d'administration développé initiallement au rezometz. Il
+# se veut agnostique au réseau considéré, de manière à être installable en
+# quelques clics.
+#
+# Copyright © 2019  Alexandre Iooss
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 from django.contrib import admin
+
+from preferences.models import GeneralOption
 
 
 class UserAdmin(admin.sites.AdminSite):
@@ -10,6 +33,14 @@ class UserAdmin(admin.sites.AdminSite):
 class ModelUserAdmin(admin.ModelAdmin):
     # Display actions on changelist bottom bu default
     actions_on_bottom = True
+
+    # Correct number of item per page
+    list_per_page = GeneralOption.get_cached_value('pagination_number')
+
+    # Custom templates
+    change_form_template = 'useradmin/change_form.html'
+    delete_confirmation_template = 'useradmin/delete_confirmation.html'
+    delete_selected_confirmation_template = 'useradmin/delete_selected_confirmation.html'
 
 
 # Defines new management site
