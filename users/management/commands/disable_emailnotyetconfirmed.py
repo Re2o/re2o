ffdb32df906d0eae155fda79c22b8ee7cb1eabca
Gabriel Detraz <detraz@crans.org>
2020-04-17 16:48:27 +0200

    Update headers

--- users/management/commands/disable_emailnotyetconfirmed.py
+++ users/management/commands/disable_emailnotyetconfirmed.py
@@ -1,6 +1,4 @@
-# Copyright © 2017  Gabriel Détraz
-# Copyright © 2017  Lara Kermarec
-# Copyright © 2017  Augustin Lemesle
+# Copyright © 2017-2020  Jean-Romain Garnier
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
