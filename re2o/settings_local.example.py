96d5b05c84b94686c9196e33e6dd0278ecaae017
Gabriel Detraz <detraz@crans.org>
2020-04-19 22:20:13 +0200

    Enforce password policy example file

--- re2o/settings_local.example.py
+++ re2o/settings_local.example.py
@@ -108,4 +108,20 @@ OPTIONNAL_APPS_RE2O = ()
 OPTIONNAL_APPS = OPTIONNAL_APPS_RE2O + ()
 
 #Set auth password validator
-AUTH_PASSWORD_VALIDATORS = []
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 8,
+        }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
