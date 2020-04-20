0b58365c872afeded8d5935e6313bf26d62ddfa7
chapeau <corentin.canebier@student-cs.fr>
2020-04-20 20:43:11 +0200

    First draft of moving api functions into each apps

---
+++ machines/api/views.py
@@ -0,0 +1,267 @@
+# -*- mode: python; coding: utf-8 -*-
+# Re2o est un logiciel d'administration développé initiallement au rezometz. Il
+# se veut agnostique au réseau considéré, de manière à être installable en
+# quelques clics.
+#
+# Copyright © 2018 Maël Kervella
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from rest_framework import viewsets, generics
+
+from . import serializers
+import machines.models as machines
+from re2o.utils import all_active_interfaces
+
+class MachineViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.Machine` objects.
+    """
+
+    queryset = machines.Machine.objects.all()
+    serializer_class = serializers.MachineSerializer
+
+
+class MachineTypeViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.MachineType` objects.
+    """
+
+    queryset = machines.MachineType.objects.all()
+    serializer_class = serializers.MachineTypeSerializer
+
+
+class IpTypeViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.IpType` objects.
+    """
+
+    queryset = machines.IpType.objects.all()
+    serializer_class = serializers.IpTypeSerializer
+
+
+class VlanViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.Vlan` objects.
+    """
+
+    queryset = machines.Vlan.objects.all()
+    serializer_class = serializers.VlanSerializer
+
+
+class NasViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.Nas` objects.
+    """
+
+    queryset = machines.Nas.objects.all()
+    serializer_class = serializers.NasSerializer
+
+
+class SOAViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.SOA` objects.
+    """
+
+    queryset = machines.SOA.objects.all()
+    serializer_class = serializers.SOASerializer
+
+
+class ExtensionViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.Extension` objects.
+    """
+
+    queryset = machines.Extension.objects.all()
+    serializer_class = serializers.ExtensionSerializer
+
+
+class MxViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.Mx` objects.
+    """
+
+    queryset = machines.Mx.objects.all()
+    serializer_class = serializers.MxSerializer
+
+
+class NsViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.Ns` objects.
+    """
+
+    queryset = machines.Ns.objects.all()
+    serializer_class = serializers.NsSerializer
+
+
+class TxtViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.Txt` objects.
+    """
+
+    queryset = machines.Txt.objects.all()
+    serializer_class = serializers.TxtSerializer
+
+
+class DNameViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.DName` objects.
+    """
+
+    queryset = machines.DName.objects.all()
+    serializer_class = serializers.DNameSerializer
+
+
+class SrvViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.Srv` objects.
+    """
+
+    queryset = machines.Srv.objects.all()
+    serializer_class = serializers.SrvSerializer
+
+
+class SshFpViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.SshFp` objects.
+    """
+
+    queryset = machines.SshFp.objects.all()
+    serializer_class = serializers.SshFpSerializer
+
+
+class InterfaceViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.Interface` objects.
+    """
+
+    queryset = machines.Interface.objects.all()
+    serializer_class = serializers.InterfaceSerializer
+
+
+class Ipv6ListViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.Ipv6List` objects.
+    """
+
+    queryset = machines.Ipv6List.objects.all()
+    serializer_class = serializers.Ipv6ListSerializer
+
+
+class DomainViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.Domain` objects.
+    """
+
+    queryset = machines.Domain.objects.all()
+    serializer_class = serializers.DomainSerializer
+
+
+class IpListViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.IpList` objects.
+    """
+
+    queryset = machines.IpList.objects.all()
+    serializer_class = serializers.IpListSerializer
+
+
+class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.Service` objects.
+    """
+
+    queryset = machines.Service.objects.all()
+    serializer_class = serializers.ServiceSerializer
+
+
+class ServiceLinkViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.Service_link` objects.
+    """
+
+    queryset = machines.Service_link.objects.all()
+    serializer_class = serializers.ServiceLinkSerializer
+
+
+class OuverturePortListViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.OuverturePortList`
+    objects.
+    """
+
+    queryset = machines.OuverturePortList.objects.all()
+    serializer_class = serializers.OuverturePortListSerializer
+
+
+class OuverturePortViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.OuverturePort` objects.
+    """
+
+    queryset = machines.OuverturePort.objects.all()
+    serializer_class = serializers.OuverturePortSerializer
+
+
+class RoleViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `machines.models.Machine` objects.
+    """
+
+    queryset = machines.Role.objects.all()
+    serializer_class = serializers.RoleSerializer
+
+
+class ServiceRegenViewSet(viewsets.ModelViewSet):
+    """Exposes list and details of the services to regen
+    """
+
+    serializer_class = serializers.ServiceRegenSerializer
+
+    def get_queryset(self):
+        queryset = machines.Service_link.objects.select_related(
+            "server__domain"
+        ).select_related("service")
+        if "hostname" in self.request.GET:
+            hostname = self.request.GET["hostname"]
+            queryset = queryset.filter(server__domain__name__iexact=hostname)
+        return queryset
+
+
+class HostMacIpView(generics.ListAPIView):
+    """Exposes the associations between hostname, mac address and IPv4 in
+    order to build the DHCP lease files.
+    """
+
+    serializer_class = serializers.HostMacIpSerializer
+
+    def get_queryset(self):
+        return all_active_interfaces()
+
+
+class SubnetPortsOpenView(generics.ListAPIView):
+    queryset = machines.IpType.objects.all()
+    serializer_class = serializers.SubnetPortsOpenSerializer
+
+
+class InterfacePortsOpenView(generics.ListAPIView):
+    queryset = machines.Interface.objects.filter(port_lists__isnull=False).distinct()
+    serializer_class = serializers.InterfacePortsOpenSerializer
+
+class DNSZonesView(generics.ListAPIView):
+    """Exposes the detailed information about each extension (hostnames,
+    IPs, DNS records, etc.) in order to build the DNS zone files.
+    """
+
+    queryset = (
+        machines.Extension.objects.prefetch_related("soa")
+        .prefetch_related("ns_set")
+        .prefetch_related("ns_set__ns")
+        .prefetch_related("origin")
+        .prefetch_related("mx_set")
+        .prefetch_related("mx_set__name")
+        .prefetch_related("txt_set")
+        .prefetch_related("srv_set")
+        .prefetch_related("srv_set__target")
+        .all()
+    )
+    serializer_class = serializers.DNSZonesSerializer
+
+
+class DNSReverseZonesView(generics.ListAPIView):
+    """Exposes the detailed information about each extension (hostnames,
+    IPs, DNS records, etc.) in order to build the DNS zone files.
+    """
+
+    queryset = machines.IpType.objects.all()
+    serializer_class = serializers.DNSReverseZonesSerializer
---
+++ topologie/api/views.py
@@ -0,0 +1,149 @@
+# -*- mode: python; coding: utf-8 -*-
+# Re2o est un logiciel d'administration développé initiallement au rezometz. Il
+# se veut agnostique au réseau considéré, de manière à être installable en
+# quelques clics.
+#
+# Copyright © 2018 Maël Kervella
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from rest_framework import viewsets, generics
+
+from . import serializers
+import topologie.models as topologie
+import machines.models as machines
+
+
+class StackViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `topologie.models.Stack` objects.
+    """
+
+    queryset = topologie.Stack.objects.all()
+    serializer_class = serializers.StackSerializer
+
+
+class AccessPointViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `topologie.models.AccessPoint` objects.
+    """
+
+    queryset = topologie.AccessPoint.objects.all()
+    serializer_class = serializers.AccessPointSerializer
+
+
+class SwitchViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `topologie.models.Switch` objects.
+    """
+
+    queryset = topologie.Switch.objects.all()
+    serializer_class = serializers.SwitchSerializer
+
+
+class ServerViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `topologie.models.Server` objects.
+    """
+
+    queryset = topologie.Server.objects.all()
+    serializer_class = serializers.ServerSerializer
+
+
+class ModelSwitchViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `topologie.models.ModelSwitch` objects.
+    """
+
+    queryset = topologie.ModelSwitch.objects.all()
+    serializer_class = serializers.ModelSwitchSerializer
+
+
+class ConstructorSwitchViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `topologie.models.ConstructorSwitch`
+    objects.
+    """
+
+    queryset = topologie.ConstructorSwitch.objects.all()
+    serializer_class = serializers.ConstructorSwitchSerializer
+
+
+class SwitchBayViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `topologie.models.SwitchBay` objects.
+    """
+
+    queryset = topologie.SwitchBay.objects.all()
+    serializer_class = serializers.SwitchBaySerializer
+
+
+class BuildingViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `topologie.models.Building` objects.
+    """
+
+    queryset = topologie.Building.objects.all()
+    serializer_class = serializers.BuildingSerializer
+
+
+class SwitchPortViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `topologie.models.Port` objects.
+    """
+
+    queryset = topologie.Port.objects.all()
+    serializer_class = serializers.SwitchPortSerializer
+
+
+class PortProfileViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `topologie.models.PortProfile` objects.
+    """
+
+    queryset = topologie.PortProfile.objects.all()
+    serializer_class = serializers.PortProfileSerializer
+
+
+class RoomViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `topologie.models.Room` objects.
+    """
+
+    queryset = topologie.Room.objects.all()
+    serializer_class = serializers.RoomSerializer
+
+
+class PortProfileViewSet(viewsets.ReadOnlyModelViewSet):
+    """Exposes list and details of `topologie.models.PortProfile` objects.
+    """
+
+    queryset = topologie.PortProfile.objects.all()
+    serializer_class = serializers.PortProfileSerializer
+
+
+class SwitchPortView(generics.ListAPIView):
+    """Output each port of a switch, to be serialized with
+    additionnal informations (profiles etc)
+    """
+
+    queryset = (
+        topologie.Switch.objects.all()
+        .select_related("switchbay")
+        .select_related("model__constructor")
+        .prefetch_related("ports__custom_profile__vlan_tagged")
+        .prefetch_related("ports__custom_profile__vlan_untagged")
+        .prefetch_related("ports__machine_interface__domain__extension")
+        .prefetch_related("ports__room")
+    )
+
+    serializer_class = serializers.SwitchPortSerializer
+
+
+class RoleView(generics.ListAPIView):
+    """Output of roles for each server
+    """
+
+    queryset = machines.Role.objects.all().prefetch_related("servers")
+    serializer_class = serializers.RoleSerializer
