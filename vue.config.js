dfddaa1d2b752f5ac5546e279ab20a85290971b1
Alexandre Iooss <erdnaxe@crans.org>
2020-04-22 11:37:08 +0200

    No need to log webpack stats

--- vue.config.js
+++ vue.config.js
@@ -7,17 +7,7 @@
  * the HelloWorld component.
  */
 
-const BundleTracker = require("webpack-bundle-tracker");
-
 module.exports = {
     // Output to Django statics
     outputDir: './static/bundles/',
-
-    // Customize Webpack    
-    chainWebpack: config => {
-        // Create file for django-webpack-loader
-        config
-            .plugin('BundleTracker')
-            .use(BundleTracker, [{ filename: './webpack-stats.json' }])
-    }
 };
