fc21a1da3e2f7c9c68c84b952703f23bac31b4e7
Jean-Romain Garnier <jean-romain.garnier@supelec.fr>
2020-04-24 15:37:05 +0200

    Add view to filter event logs

--- logs/forms.py
+++ logs/forms.py
@@ -25,15 +25,68 @@ from django.forms import Form
 from django.utils.translation import ugettext_lazy as _
 from re2o.base import get_input_formats_help_text
 
+import inspect
+
+# Import all models in which there are classes to be filtered on
+import machines.models
+import preferences.models
+import tickets.models
+import topologie.models
+import users.models
+
+
+def get_classes(module):
+    classes = []
+
+    for name, obj in inspect.getmembers(module):
+        if inspect.isclass(obj):
+            classes.append((obj, name))
+
+    return classes
+
+
+# Get the list of all imported classes
+modules = [machines.models, preferences.models, tickets.models, topologie.models, users.models]
+classes = sum([get_classes(m) for m in modules])
+
+CHOICES_ACTION_TYPE = classes
+
 CHOICES_TYPE = (
     ("ip", _("IPv4")),
     ("mac", _("MAC address")),
 )
 
 
+class ActionsSearchForm(Form):
+    """The form for a simple search"""
+    u = forms.CharField(
+        label=_("Performed by"),
+        max_length=100,
+        required=False,
+        queryset=users.models.User.objects.all()
+    )
+    t = forms.MultipleChoiceField(
+        label=_("Action type"),
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        choices=CHOICES_ACTION_TYPE,
+        initial=[i[0] for i in CHOICES_ACTION_TYPE],
+    )
+    s = forms.DateField(required=False, label=_("Start date"))
+    e = forms.DateField(required=False, label=_("End date"))
+
+    def __init__(self, *args, **kwargs):
+        super(MachineHistorySearchForm, self).__init__(*args, **kwargs)
+        self.fields["s"].help_text = get_input_formats_help_text(
+            self.fields["s"].input_formats
+        )
+        self.fields["e"].help_text = get_input_formats_help_text(
+            self.fields["e"].input_formats
+        )
+
+
 class MachineHistorySearchForm(Form):
     """The form for a simple search"""
-
     q = forms.CharField(
         label=_("Search"),
         max_length=100,
