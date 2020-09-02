e4d40a876c3b73153e992e37bf9bb04a13cf93a7
histausse <mineau.jean.marie@gmail.com>
2020-09-02 19:14:27 +0200

    add the endpoint 'api/multi_op/pending-connection'

---
+++ multi_op/api/views.py
@@ -0,0 +1,38 @@
+# -*- mode: python; coding: utf-8 -*-
+# Re2o est un logiciel d'administration développé initiallement au rezometz. Il
+# se veut agnostique au réseau considéré, de manière à être installable en
+# quelques clics.
+#
+# Copyright © 2020 Mineau Jean-Marie
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from rest_framework import viewsets
+
+from . import serializers
+import topologie.models as topologie
+
+class PendinfConnectionViewSet(viewsets.ReadOnlyModelViewSet):
+    """Expose list and detail of `topologie.models.Room` object
+       waiting to be connected.
+    """
+
+    queryset = (
+        topologie.Room.objects.select_related("building__dormitory")
+        .filter(port__isnull=True)
+        .filter(adherent__in=all_has_access())
+        .order_by("building_dormitory", "port")
+    )
+    serializer_class = serializers.RoomSerializer
