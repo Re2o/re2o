1bc5b5143983a12a69ea84de09e542a2ba75c5bd
chapeau <corentin@canebier.fr>
2020-11-29 22:37:42 +0100

    a few small fixes

--- re2o/mixins.py
+++ re2o/mixins.py
@@ -5,6 +5,7 @@
 #
 # Copyright © 2018  Gabriel Détraz
 # Copyright © 2017  Charlie Jacomme
+# Copyright © 2020  Corentin Canebier
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
