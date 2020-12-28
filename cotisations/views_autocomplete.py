f4dbec01c8e1ea709211e899758496c2ec998961
chirac <chirac@re2o-test.auro.re>
2020-12-28 18:57:08 +0100

    Add autocomplete on cotisations

---
+++ cotisations/views_autocomplete.py
@@ -0,0 +1,50 @@
+# -*- mode: python; coding: utf-8 -*-
+# Re2o est un logiciel d'administration développé initiallement au Rézo Metz. Il
+# se veut agnostique au réseau considéré, de manière à être installable en
+# quelques clics.
+#
+# Copyright © 2017-2020  Gabriel Détraz
+# Copyright © 2017-2020  Jean-Romain Garnier
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# App de gestion des users pour re2o
+# Lara Kermarec, Gabriel Détraz, Lemesle Augustin
+# Gplv2
+"""
+Django views autocomplete view
+
+Here are defined the autocomplete class based view.
+
+"""
+from __future__ import unicode_literals
+
+from django.db.models import Q, Value, CharField
+
+from .models import (
+    Banque
+)
+
+from re2o.mixins import AutocompleteViewMixin
+
+from re2o.acl import (
+    can_view_all,
+)
+
+
+class BanqueAutocomplete(AutocompleteViewMixin):
+    obj_type = Banque
+
+
