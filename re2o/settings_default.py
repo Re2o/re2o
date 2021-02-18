e0ca9eb38029af1b4d4d6da6e5644221a90cdf72
Yoann Piétri <me@nanoy.fr>
2021-02-18 18:41:51 +0100

    fix: :pencil2: locale != local

--- re2o/settings_default.py
+++ re2o/settings_default.py
@@ -20,7 +20,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-"""re2o.settings_locale
+"""re2o.settings_local
 The file with all the available options for a locale configuration of re2o
 """
 
